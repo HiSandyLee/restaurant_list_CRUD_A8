04194c1ea1b1b1a34ec025345d93b95d194f156b
Sandy Lee <wasd86910@gmail.com>
2021-01-17 21:39:16 +0800

    載入 Bootstrap 與 jQuery並套用Bootstrap

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 const port = 3000
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('My restaurant list hw')
+  res.render('index')
 })
 
 
 app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
+  console.log(`Express is listening on http://localhost:${port}`)
 })
