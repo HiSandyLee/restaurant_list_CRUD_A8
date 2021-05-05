cd1dff5f8b86bf1a1f82b2a0c9e3bff9a423d895
sandylee <wasd86910@gmail.com>
2021-05-05 23:17:29 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,50 +1,23 @@
-// require packages used in the project
-const express = require('express')
-// require handlebars in the project
-const exphbs = require('express-handlebars')
+const express = require('express')// require packages used in the project
+const exphbs = require('express-handlebars')// require handlebars in the project
 const restaurantList = require('./restaurant.json')
-const mongoose = require('mongoose') // 載入 mongoose
-const Restaurant = require('./models/restaurant')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')// 載入 method-override
+
 const routes = require('./routes')
-const app = express()
+require('./config/mongoose')
+
 const port = 3000
-
-
-mongoose.connect('mongodb://localhost/restaurant-list', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})// 設定連線到 mongoDB
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-//once只會執行一次
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+const app = express()
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))// setting static files
+app.use(methodOverride('_method'))// 設定每一筆請求都會透過 methodOverride 進行前置處理
+app.use(routes)// routes setting
 
-// setting static files
-app.use(express.static('public'))
-
-// 設定每一筆請求都會透過 methodOverride 進行前置處理
-app.use(methodOverride('_method'))
-
-// routes setting
-//home page
-app.use(routes)
-
-
-
-app.listen(port, () => {
+app.listen(3000, () => {
   console.log(`Express is listening on http://localhost:${port}`)
 })
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,19 +1,8 @@
-const mongoose = require('mongoose') // 載入 mongoose
+const db = require('../../config/mongoose')
 const Restaurant = require('../restaurant')
 const restaurantList = require('../../restaurant.json').results
 
-mongoose.connect('mongodb://localhost/restaurant-list', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
 db.once('open', () => {
-  console.log('mogodb connected!')
   restaurantList.forEach(restaurant => Restaurant.create(restaurant))
   console.log('done')
 })
